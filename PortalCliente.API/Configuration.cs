a965e513e31259c5575f4a49cc1f4faa208dc7b4
Lucas Cecchin <lucas@elevor.com>
2024-01-10 17:00:36 -0300

    .

--- PortalCliente.API/Configuration.cs
+++ PortalCliente.API/Configuration.cs
@@ -1,10 +1,22 @@
-﻿namespace PortalCliente.API
+﻿using Microsoft.AspNetCore.Mvc;
+using PortalCliente.API.Attibutes;
+
+namespace PortalCliente.API
 {
     public static class Configuration
     {
         //TOKEN - JWT - Json Web Token
         public static string JwtKey = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
 
+
+        //É possível criar tokens genericos, fazendo isso é só colocar o atributo [ApiKey], na classe ou metodo do controller
+        //A classe ApiKeyAttribute é a responsavel por fazer esse controle.
+        //Exemplo:
+        //  [ApiKey]
+        //  public IActionResult Get()
+        //  {
+        //        return Ok();
+        //  }
         public static string ApiKeyName = "api_key";
         public static string ApiKey = "curso_Api";
 
